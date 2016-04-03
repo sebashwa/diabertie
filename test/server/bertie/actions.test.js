4dd053099f3826e35a057d0762fe2e03108efef6
Sebastian Warmbrunn <sebastianwarmbrunn@gmail.com>
2016-04-03 05:24:07 +0200

    Do not use .env file for tests
    The contents were not secret anyways

--- test/server/bertie/actions.test.js
+++ test/server/bertie/actions.test.js
@@ -3,7 +3,7 @@ import { connectBot } from '../../../server/bertie/actions';
 import User from '../../../server/models/User';
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGO_DB_URL, (err) => { if (err) throw err; } );
+mongoose.connect('mongodb://localhost:27017/diabertie_test', (err) => { if (err) throw err; } );
 
 describe('telegram actions', () => {
   describe('#connectBot', () => {
