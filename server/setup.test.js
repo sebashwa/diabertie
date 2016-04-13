a798590bb91edab6965619991f993fb399a1523c
Sebastian Warmbrunn <sebastianwarmbrunn@gmail.com>
2016-04-13 07:30:49 +0200

    Clean up the database before running tests.

--- server/setup.test.js
+++ server/setup.test.js
@@ -1,2 +1,9 @@
+import * as models from './models';
 import mongoose from 'mongoose';
-mongoose.connect('mongodb://localhost:27017/diabertie_test', (err) => { if (err) throw err; } );
+
+before(async () => {
+  await mongoose.connect('mongodb://localhost:27017/diabertie_test', (err) => { if (err) throw err; } );
+  await Promise.all(
+    Object.keys(models).map(m => models[m].find({}).remove())
+  );
+});
