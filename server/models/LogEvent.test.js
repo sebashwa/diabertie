44899fc9beb6b09068268e64a6d20aae2768908c
Sebastian Warmbrunn <sebastianwarmbrunn@gmail.com>
2016-04-13 08:40:18 +0200

    Use utc time when inserting logEvents in test
    It is also used when storing to the database

--- server/models/LogEvent.test.js
+++ server/models/LogEvent.test.js
@@ -22,8 +22,8 @@ describe('LogEvent', () => {
     let user;
 
     beforeEach(async () => {
-      user = await User.create({ email: 'true@user' });
-      time = moment('02-05-2015 22:15', 'MM-DD-YYYY HH:mm').tz(user.timezone);
+      user = await User.create({ email: 'true@user', timezone: 'Europe/Berlin' });
+      time = moment.utc('02-05-2015 22:15', 'MM-DD-YYYY HH:mm').tz(user.timezone);
 
       logEventTime = await LogEvent.create({ createdAt: time, user: user.id});
       logEventTimePlusTwo = await LogEvent.create({ createdAt: time.clone().add(2, 'minutes'), user: user.id });
@@ -38,7 +38,7 @@ describe('LogEvent', () => {
       const aggregation = await LogEvent.groupInInterval(time, user, 15);
 
       expect(aggregation, 'to satisfy',
-        [{ _id: { hour: 21, minute: 15 }, logEvents: Array }]
+        [{ _id: { hour: 22, minute: 15 }, logEvents: Array }]
       );
 
       expect(aggregation[0].logEvents.map(e => e._id), 'to equal', [
@@ -68,8 +68,8 @@ describe('LogEvent', () => {
       const aggregation = await LogEvent.groupInInterval(time, user);
 
       expect(aggregation, 'to satisfy', [
-        { _id: { hour: 21, minute: 20 }, logEvents: Array },
-        { _id: { hour: 21, minute: 15 }, logEvents: Array }
+        { _id: { hour: 22, minute: 20 }, logEvents: Array },
+        { _id: { hour: 22, minute: 15 }, logEvents: Array }
       ]);
 
       expect(aggregation[0].logEvents.map(e => e._id), 'to equal', [
