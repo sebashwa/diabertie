c646abf55536a14a381b04dc8f76d9389687ab95
Sebastian Warmbrunn <sebastianwarmbrunn@gmail.com>
2016-04-14 17:33:53 +0200

    Add PropTypes validation for dumb components

--- shared/components/Introduction.jsx
+++ shared/components/Introduction.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
-export default ({ user, botName }) => {
+function Introduction ({ user, botName }) {
   return (
     <a href={ `http://telegram.me/${botName}?start=${user.get('telegramToken')}` } target="_blank">Connect</a>
   );
 };
+
+Introduction.propTypes = {
+  user:    PropTypes.object.isRequired,
+  botName: PropTypes.string.isRequired
+};
+
+export default Introduction;
--- shared/components/LogbookData.jsx
+++ shared/components/LogbookData.jsx
@@ -1,35 +1,48 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import moment from 'moment-timezone';
 import { logEventStyle, logbookDataStyle } from './LogbookData.style';
 
-export default ({ logEvents: logEventBundles, timezone }) => {
+function LogBookData({ logEvents: logEventBundles, timezone }) {
   return (
     <div className="logbookData" style={ logbookDataStyle() }>
-      { logEventBundles.size > 0 ?
-        logEventBundles.map((logEventBundle, i) => {
-        const { _id: time , logEvents } = logEventBundle.toObject();
-        const { hour, minute } = time.toObject();
+      {
+        logEventBundles.size > 0 ?
+          logEventBundles.map((logEventBundle, i) => {
+            const { _id: time , logEvents } = logEventBundle.toObject();
+            const { hour, minute } = time.toObject();
 
-        return (
-          <div key={i} className="logEventBundle" style={ logEventStyle() }>
-            <span>{ moment.utc(`${hour}:${minute}`, 'HH:mm').tz(timezone).format('HH:mm') } </span>
+            return (
+              <div key={i} className="logEventBundle" style={ logEventStyle() }>
+                <span>{ moment.utc(`${hour}:${minute}`, 'HH:mm').tz(timezone).format('HH:mm') } </span>
 
 
-            { ['sugar', 'food', 'therapy'].map((type) =>
-                logEvents.filter(e => e.get('category') === type).map(logEvent => {
-                  const { originalValue, originalUnit } = logEvent.toObject();
+                {
+                  ['sugar', 'food', 'therapy'].map((type) =>
+                    logEvents.filter(e => e.get('category') === type).map(logEvent => {
+                      const { originalValue, originalUnit } = logEvent.toObject();
 
-                  return(
-                    <div className="logEvent">
-                      <span>{ originalValue } </span>
-                      <span>{ originalUnit }</span>
-                    </div>
-                );
-              })
-            )}
-          </div>
-        );
-      }) : 'NO DATA TO SEE HERE' }
+                      return(
+                        <div className="logEvent">
+                          <span>{ originalValue } </span>
+                          <span>{ originalUnit }</span>
+                        </div>
+                      );
+                    })
+                  )
+                }
+              </div>
+            );
+          })
+        :
+        'NO DATA TO SEE HERE'
+      }
     </div>
   );
 };
+
+LogBookData.propTypes = {
+  logEvents: PropTypes.object.isRequired,
+  timezone:  PropTypes.string.isRequired
+};
+
+export default LogBookData;
