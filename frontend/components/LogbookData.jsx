fa13eec8dae74575f7ff16badd9c873e857e5eef
Sebastian Warmbrunn <sebastianwarmbrunn@gmail.com>
2016-05-02 19:05:16 +0200

    Add a bit more styling to LogbookData

--- frontend/components/LogbookData.jsx
+++ frontend/components/LogbookData.jsx
@@ -3,15 +3,10 @@ import moment from 'moment-timezone';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './LogbookData.css';
 
+import Time from 'images/Time';
 import Sugar from 'images/Sugar';
 import Food from 'images/Food';
 import Therapy from 'images/Therapy';
-
-const icons = {
-  sugar:   <Sugar className={styles.icon} />,
-  food:    <Food className={styles.icon} />,
-  therapy: <Therapy className={styles.icon} />
-};
 
 function LogbookData({ logEvents: logEventBundles, user, p }) {
   return (
@@ -22,42 +17,48 @@ function LogbookData({ logEvents: logEventBundles, user, p }) {
       </span>
     </div>
     :
-    <div className={styles.root} >
-      <div className={styles.row}>
-        <div>{"Time"}</div>
-        {
-          logEventBundles.map((logEventBundle, i) => {
-            const { _id: time } = logEventBundle.toObject();
-            const { hour, minute } = time.toObject();
-            const formattedTime = moment.utc(`${hour}:${minute}`, 'HH:mm').tz(user.get('timezone')).format('HH:mm');
+    <div className={styles.root}>
+      <div className={styles.icons}>
+        <Time className={styles.icon}/>
+        <Sugar className={styles.icon} />
+        <Food className={styles.icon} />
+        <Therapy className={styles.icon} />
+      </div>
+      <div className={styles.rows} >
+        <div className={styles.row}>
+          {
+            logEventBundles.map((logEventBundle, i) => {
+              const { _id: time } = logEventBundle.toObject();
+              const { hour, minute } = time.toObject();
+              const formattedTime = moment.utc(`${hour}:${minute}`, 'HH:mm').tz(user.get('timezone')).format('HH:mm');
 
-            return (<div><b key={i}>{ formattedTime }</b></div>);
+              return (<div><b key={i}>{ formattedTime }</b></div>);
+            })
+          }
+        </div>
+        {
+          ['sugar', 'food', 'therapy'].map(type => {
+            return (
+              <div className={ styles.row }>
+                {
+                  logEventBundles.map((logEventBundle, i) => {
+                    const { logEvents } = logEventBundle.toObject();
+                    return (
+                      <div key={i} className={ styles.logEventBundle } >
+                        {
+                          logEvents.filter(e => e.get('category') === type).map(logEvent =>
+                            <div>{ logEvent.get('originalValue') }</div>
+                          )
+                        }
+                      </div>
+                    );
+                  })
+                }
+              </div>
+            );
           })
         }
       </div>
-      {
-        ['sugar', 'food', 'therapy'].map(type => {
-          return (
-            <div className={ styles.row }>
-              <div>{ icons[type] }</div>
-              {
-                logEventBundles.map((logEventBundle, i) => {
-                  const { logEvents } = logEventBundle.toObject();
-                  return (
-                    <div key={i} className={ styles.logEventBundle } >
-                      {
-                        logEvents.filter(e => e.get('category') === type).map(logEvent =>
-                          <div>{ logEvent.get('originalValue') }</div>
-                        )
-                      }
-                    </div>
-                  );
-                })
-              }
-            </div>
-          );
-        })
-      }
     </div>
   );
 };
