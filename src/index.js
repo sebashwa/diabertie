813d4fbdfda1d5615d0be8837385f5feade765ec
Sebastian Warmbrunn <sebastianwarmbrunn@gmail.com>
2016-09-26 14:38:31 +0200

    Use ES6 native promises for mongoose

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import TelegramBot from 'node-telegram-bot-api';
 import bearBertie from './bertie';
 
+mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGO_DB_URL, (err) => {
   if (err) console.log('Could not connect to mongodb.');
 });
