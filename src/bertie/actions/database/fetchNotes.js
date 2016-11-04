91c40c68997b69c1af858e338ed95dac56be6a23
Sebastian Warmbrunn <sebastianwarmbrunn@gmail.com>
2016-11-04 07:42:23 +0100

    Fix severe privacy bug
    Scoping notes on user ...

--- src/bertie/actions/database/fetchNotes.js
+++ src/bertie/actions/database/fetchNotes.js
@@ -6,7 +6,7 @@ export default async (user, date) => {
     const start = date.clone().tz(user.timezone).startOf('day');
     const end = date.clone().tz(user.timezone).endOf('day');
 
-    const data = await Note.find({ forDate: { $lt: end, $gt: start } });
+    const data = await Note.find({ forDate: { $lt: end, $gt: start }, user: user.id });
 
     return { data };
   } catch (e) {
